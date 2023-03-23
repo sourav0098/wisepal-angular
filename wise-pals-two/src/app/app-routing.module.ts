5b987b242131b4e05b09a1b9f2f0ebb1d263f48a
jgutierrezo <jgutierrezoa2@gmail.com>
2023-03-22 21:07:19 -0400

    routing for login and register

--- wise-pals-two/src/app/app-routing.module.ts
+++ wise-pals-two/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './authentication/login/login.component';
+import { RegisterComponent } from './authentication/register/register.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: '', component:HomeComponent },
+  { path: 'login', component:LoginComponent },
+  { path: 'register', component:RegisterComponent },
 ];
 
 @NgModule({
