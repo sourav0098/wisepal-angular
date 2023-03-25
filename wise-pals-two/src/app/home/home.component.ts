9b921274208ac4fefea2529dfb7d9e763fdc453f
jgutierrezo <jgutierrezoa2@gmail.com>
2023-03-25 18:16:49 -0400

    login screen

--- wise-pals-two/src/app/home/home.component.ts
+++ wise-pals-two/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { API_ENDPOINTS } from '../../util/apiEndpoints';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +9,18 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  result: any;
+  content= ''
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.http.get(`${API_ENDPOINTS.BASE_URL}${API_ENDPOINTS.CONTENTS}/63e7a5d14236e852403de3c6`)
+      .subscribe(item => {
+        this.result = item;
+        console.log('response: ', this.result);
+        this.content = this.result.data.description;
+      });
+  }
+  
 }
